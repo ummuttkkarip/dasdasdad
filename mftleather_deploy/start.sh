99332d7ca74a20399bea0afb662b2eb43971276f
Umut Karip <109227859+ummuttkkarip@users.noreply.github.com>
2025-09-05 01:38:11 +0300

    Delete mftleather_deploy directory

--- mftleather_deploy/start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Create tables if they don't exist
-python -c "from api.database import create_tables; create_tables()"
-
-# Start the FastAPI application
-uvicorn api.fastapi_app:app --host 0.0.0.0 --port $PORT
